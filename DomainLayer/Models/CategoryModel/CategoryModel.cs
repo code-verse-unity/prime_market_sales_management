c4ceca636becea4fa4d02957da39c8ee76fe0f3d
RASOLOMANANA Olivier <herimanitraolivierr@gmail.com>
2023-08-02 06:01:56 +0300

    feat: add domain layer

---
+++ DomainLayer/Models/CategoryModel/CategoryModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DomainLayer.Models.CategoryModel
+{
+    public class CategoryModel : ICategoryModel
+    {
+        public int Id { get; set; }
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Category name is required")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Category ray number is required")]
+        public int RayNumber { get; set; }
+    }
+}
