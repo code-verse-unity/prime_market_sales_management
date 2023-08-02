8a5311eb01c7ac4a71e9991874677ab7d3fe3624
RASOLOMANANA Olivier <herimanitraolivierr@gmail.com>
2023-08-02 06:01:15 +0300

    feat: add infrastructure layer

---
+++ InfrastructureLayer/Repositories/CategoryRepository/CategoryRepository.cs
@@ -0,0 +1,32 @@
+﻿using DomainLayer.Models.CategoryModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InfrastructureLayer.Repositories.CategoryRepository
+{
+    public class CategoryRepository : ICategoryRepository
+    {
+        public void Add(ICategoryModel category)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(ICategoryModel category)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<ICategoryModel> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(ICategoryModel category)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ InfrastructureLayer/Repositories/CategoryRepository/ICategoryRepository.cs
@@ -0,0 +1,17 @@
+﻿using DomainLayer.Models.CategoryModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InfrastructureLayer.Repositories.CategoryRepository
+{
+    public interface ICategoryRepository
+    {
+        void Add(ICategoryModel category);
+        void Delete(ICategoryModel category);
+        void Update(ICategoryModel category);
+        IEnumerable<ICategoryModel> GetAll();
+    }
+}
