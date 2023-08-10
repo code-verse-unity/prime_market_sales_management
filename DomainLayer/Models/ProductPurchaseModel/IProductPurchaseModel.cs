19cc103d7c95325b020d435067a2bd853529b916
Ismael-Rakotondrazaka <ismaelrakotondrazaka@gmail.com>
2023-08-10 10:50:12 +0300

    feat(ProductPurchaseModel.cs): Create the ProductPurchaseModel model
    feat(IProductPurchaseModel.cs): Create an interface for the product purchase model

---
+++ DomainLayer/Models/ProductPurchaseModel/IProductPurchaseModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DomainLayer.Models.ProductPurchaseModel
+{
+    public interface IProductPurchaseModel
+    {
+        int Id { get; set; }
+        int ProductId { get; set; }
+        int PurchaseId { get; set; }
+        int Quantity { get; set; }
+        double Price { get; set; }
+    }
+}
